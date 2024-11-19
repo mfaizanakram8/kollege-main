fc48929cf2f1a7681db32af559ce052861e9d5e5
mfaizanakram8 <mfaizanakram536@gmail.com>
2024-11-19 20:13:10 +0500

    update

--- src/config/api/axios.js
+++ src/config/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3500",
-  headers: { "Content-Type": "application/json" },
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3500',
   withCredentials: true
 });
